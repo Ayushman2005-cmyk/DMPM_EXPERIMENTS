2ff4784834b1ec244c5b7393988740c3fe99023d
24CSEAIML003 AYUSHMAN KAR <24cseaiml003.ayushmankar@giet.edu>
2026-07-09 17:41:29 +0530

    Add files via upload

---
+++ EXPT-1/Prog5.py
@@ -0,0 +1,13 @@
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+d = input("Enter the arithmatic operation(+,-,*,/):")
+if d == '+':
+    print(a+b)
+elif d == '-':
+    print(a-b)
+elif d == '*':
+    print(a*b)
+elif d == '/':
+    print(a/b)
+else:
+    print("Verify")
